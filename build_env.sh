ea2a6ea3968da3340b11da6ac4ea3e338adc58da
null <null@sl7-base.0x00.cab>
2019-02-10 20:49:37 -0500

    reverted to sudo in script

--- build_env.sh
+++ build_env.sh
@@ -1,17 +1,13 @@
-until [[ -d ${usersHome} ]]; do
-    read -r -p "Enter user's home directory: " usersHome
-done
-
 # Clean env
-yum -y remove vim
-rm -rf $usersHome/.vim*
+sudo yum -y remove vim
+rm -rf ~/.vim*
 
 # build with python support
-yum -y install epel-release
-yum -y update
+sudo yum -y install epel-release
+sudo yum -y update
 
 # Install packages
-yum -y install python34 python34-devel gcc ncurses ncurses-devel python python-devel ruby ruby-devel lua lua-devel luajit luajit-devel ctags tcl-devel perl perl-devel perl-ExtUtils-ParseXS perl-ExtUtils-XSpp perl-ExtUtils-CBuilder perl-ExtUtils-Embed mono-devel golang golang-src npm cargo cmake
+sudo yum -y install python34 python34-devel gcc ncurses ncurses-devel python python-devel ruby ruby-devel lua lua-devel luajit luajit-devel ctags tcl-devel perl perl-devel perl-ExtUtils-ParseXS perl-ExtUtils-XSpp perl-ExtUtils-CBuilder perl-ExtUtils-Embed mono-devel golang golang-src npm cargo cmake
 
 # Build latest VIM from src
 git clone https://github.com/vim/vim.git /usr/src/vim
@@ -27,25 +23,25 @@ cd /usr/src/vim
     --enable-luainterp=yes \
     --enable-gui=gtk2 \
     --enable-cscope \
-    --prefix=/usr/local \ 
+    --prefix=/usr/local \
     --enable-fail-if-missing
-make install
-ln -s /usr/local/bin/vim /usr/bin/vim
+sudo make install
+sudo ln -s /usr/local/bin/vim /usr/bin/vim
 
-git clone https://github.com/VundleVim/Vundle.vim.git $usersHome/.env/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.env/.vim/bundle/Vundle.vim
 
-rm -rf $usersHome/.vim/
-rm -rf $usersHome/.vimrc
-rm -rf $usersHome/.viminfo
-ln -s $usersHome/.env/.vim/ $usersHome/.vim
-ln -s $usersHome/.env/.vimrc $usersHome/.vimrc
-ln -s $usersHome/.env/.viminfo $usersHome/.viminfo
-rm -rf $usersHome/.bashrc
-rm -rf $usersHome/.bash_logout
-rm -rf $usersHome/.bash_profile
-ln -s $usersHome/.env/.bashrc $usersHome/.bashrc
-ln -s $usersHome/.env/.bash_logout $usersHome/.bash_logout
-ln -s $usersHome/.env/.bash_profile $usersHome/.bash_profile
+rm -rf ~/.vim/
+rm -rf ~/.vimrc
+rm -rf ~/.viminfo
+ln -s ~/.env/.vim/ ~/.vim
+ln -s ~/.env/.vimrc ~/.vimrc
+ln -s ~/.env/.viminfo ~/.viminfo
+rm -rf ~/.bashrc
+rm -rf ~/.bash_logout
+rm -rf ~/.bash_profile
+ln -s ~/.env/.bashrc ~/.bashrc
+ln -s ~/.env/.bash_logout ~/.bash_logout
+ln -s ~/.env/.bash_profile ~/.bash_profile
 
 echo 'When VIM launches execute ":VundleInstall"'
 
@@ -56,5 +52,5 @@ done
 
 vim
 
-cd $usersHome/.env/.vim/bundle/YouCompleteMe
+cd ~/.env/.vim/bundle/YouCompleteMe
 ./install.py --all
