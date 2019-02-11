8ae42e25b6101ae52a53c7be7e5d7463ff608c24
null <null@sl7-base.0x00.cab>
2019-02-10 19:47:12 -0500

    removed temp testing sudos

--- build_env.sh
+++ build_env.sh
@@ -1,21 +1,21 @@
 # Clean env
-sudo yum -y remove vim
+yum -y remove vim
 rm -rf ~/.vim*
 
 
 # build with python support
-sudo yum -y install epel-release
-sudo yum -y update
+yum -y install epel-release
+yum -y update
 
 
 # Install packages
-sudo yum -y install python34 python34-devel gcc ncurses ncurses-devel python python-devel ruby ruby-devel lua lua-devel luajit luajit-devel ctags tcl-devel perl perl-devel perl-ExtUtils-ParseXS perl-ExtUtils-XSpp perl-ExtUtils-CBuilder perl-ExtUtils-Embed mono-devel golang golang-src npm cargo cmake
+yum -y install python34 python34-devel gcc ncurses ncurses-devel python python-devel ruby ruby-devel lua lua-devel luajit luajit-devel ctags tcl-devel perl perl-devel perl-ExtUtils-ParseXS perl-ExtUtils-XSpp perl-ExtUtils-CBuilder perl-ExtUtils-Embed mono-devel golang golang-src npm cargo cmake
 
 
 # Build latest VIM from src
-sudo git clone https://github.com/vim/vim.git /usr/src/vim
+git clone https://github.com/vim/vim.git /usr/src/vim
 cd /usr/src/vim
-sudo ./configure --with-features=huge \
+./configure --with-features=huge \
     --enable-multibyte \
     --enable-rubyinterp=yes \
     --enable-pythoninterp=yes \
@@ -28,8 +28,8 @@ sudo ./configure --with-features=huge \
     --enable-cscope \
     --prefix=/usr/local \ 
     --enable-fail-if-missing
-sudo make install
-sudo ln -s /usr/local/bin/vim /usr/bin/vim
+make install
+ln -s /usr/local/bin/vim /usr/bin/vim
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.env/.vim/bundle/Vundle.vim
 
@@ -47,13 +47,12 @@ cd ~/.env/.vim/bundle/YouCompleteMe
 rm -rf ~/.vim/
 rm -rf ~/.vimrc
 rm -rf ~/.viminfo
-sudo ln -s ~/.env/.vim/ ~/.vim
-sudo ln -s ~/.env/.vimrc ~/.vimrc
-sudo ln -s ~/.env/.viminfo ~/.viminfo
+ln -s ~/.env/.vim/ ~/.vim
+ln -s ~/.env/.vimrc ~/.vimrc
+ln -s ~/.env/.viminfo ~/.viminfo
 rm -rf ~/.bashrc
 rm -rf ~/.bash_logout
 rm -rf ~/.bash_profile
-sudo ln -s ~/.env/.bashrc ~/.bashrc
-sudo ln -s ~/.env/.bash_logout ~/.bash_logout
-sudo ln -s ~/.env/.bash_profile ~/.bash_profile
-
+ln -s ~/.env/.bashrc ~/.bashrc
+ln -s ~/.env/.bash_logout ~/.bash_logout
+ln -s ~/.env/.bash_profile ~/.bash_profile
